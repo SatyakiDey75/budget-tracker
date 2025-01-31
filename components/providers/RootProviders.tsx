297ba8988d2062b3ea9bb51cebafff1586748c69
SatyakiDey75 <sdey2836@gmail.com>
2025-01-31 20:39:36 +0530

    wizard done

--- components/providers/RootProviders.tsx
+++ components/providers/RootProviders.tsx
@@ -2,14 +2,24 @@
 
 import { ThemeProvider } from "next-themes";
 import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function RootProviders({ children }: { children: React.ReactNode }) {
-    return <ThemeProvider 
+    
+    const [ queryClient ] = React.useState(() => new QueryClient({}));
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ThemeProvider 
                 attribute="class"
                 // defaultTheme="light"
                 enableSystem
                 disableTransitionOnChange
             >
                 {children}
-            </ThemeProvider>;
+            </ThemeProvider>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
 }
