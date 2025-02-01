3010ea70d0b65d1f43ef37a2a7771d3986164abb
SatyakiDey75 <sdey2836@gmail.com>
2025-02-02 01:56:35 +0530

    CreateTransactions done

--- app/(dashboard)/_components/CreateTransactionDialog.tsx
+++ app/(dashboard)/_components/CreateTransactionDialog.tsx
@@ -168,7 +168,15 @@ export default function CreateTransactionDialog({ trigger, type }: Props) {
                                                 </FormControl>
                                             </PopoverTrigger>
                                             <PopoverContent className="w-auto p-0">
-                                                <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
+                                                <Calendar
+                                                    mode="single"
+                                                    selected={field.value}
+                                                    onSelect={(value) => {
+                                                        if (!value) return;
+                                                        field.onChange(value);
+                                                    }}
+                                                    initialFocus
+                                                />
                                             </PopoverContent>
                                         </Popover>
                                         <FormDescription>
